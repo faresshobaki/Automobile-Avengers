f3b2ccebd357b61d242db489365e00bda0274b5f
IbrahimAlj <ibrahimajanabi2@gmail.com>
2024-11-19 18:09:35 -0800

    Ibrahim

---
+++ src/main/java/com/example/BackendAMA/BackendAMAApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendAMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAMAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAMAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackendAMA/BackendAMAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendAMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAMAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
